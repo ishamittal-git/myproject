3adaa00cc70ae2e32ce73090ab9f2a2cf9e8fdbc
Ishaan Mittal <ishaanmittal123@gmail.com>
2025-04-03 13:38:18 +0530

    add description

--- myproject/edible/models.py
+++ myproject/edible/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class CutleryFactory(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, default='')
     phone_number = models.IntegerField(null=True)
     
     def __str__(self):
